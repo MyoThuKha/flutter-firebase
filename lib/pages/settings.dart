53d4b1fdea7ec4851862113a1d0b2808e30aefb1
MyoThuKha <myothukha.dev@gmail.com>
2022-12-05 18:32:04 +0630

    added settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,12 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
+  Future logout() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("settings")),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: GestureDetector(
+              onTap: () => logout(),
+              child: const ListTile(
+                title: Text("Log out"),
+                trailing: (Icon(
+                  Icons.logout_rounded,
+                  color: Colors.black,
+                )),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
