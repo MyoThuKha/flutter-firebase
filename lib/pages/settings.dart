ebb97594de4bdf0f3ee55d5289a023a2a4b6042e
MyoThuKha <myothukha.dev@gmail.com>
2022-12-06 21:12:43 +0630

    added delete ui

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,10 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   Future logout() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future delete(String id) async {
+    await FirebaseFirestore.instance.collection("users").doc(id).delete();
     await FirebaseAuth.instance.signOut();
   }
 
@@ -25,6 +32,23 @@ class SettingPage extends StatelessWidget {
                 title: Text("Log out"),
                 trailing: (Icon(
                   Icons.logout_rounded,
+                  color: Colors.black,
+                )),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: const ListTile(
+                title: Text("Delete Account"),
+                trailing: (Icon(
+                  CupertinoIcons.trash,
                   color: Colors.black,
                 )),
               ),
