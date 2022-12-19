76dab642d10222fb0a6e724de1e5311c3cb893e0
MyoThuKha <myothukha.dev@gmail.com>
2022-12-19 09:38:06 +0630

    ...

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -10,10 +10,10 @@ class SettingPage extends StatelessWidget {
     await FirebaseAuth.instance.signOut();
   }
 
-  Future delete(String id) async {
-    await FirebaseFirestore.instance.collection("users").doc(id).delete();
-    await FirebaseAuth.instance.signOut();
-  }
+  // Future delete(String id) async {
+  //   await FirebaseFirestore.instance.collection("users").doc(id).delete();
+  //   await FirebaseAuth.instance.signOut();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +37,23 @@ class SettingPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-            decoration: BoxDecoration(
-              color: Colors.grey[400],
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: GestureDetector(
-              onTap: () {},
-              child: const ListTile(
-                title: Text("Delete Account"),
-                trailing: (Icon(
-                  CupertinoIcons.trash,
-                  color: Colors.black,
-                )),
-              ),
-            ),
-          )
+          // Container(
+          //   margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          //   decoration: BoxDecoration(
+          //     color: Colors.grey[400],
+          //     borderRadius: BorderRadius.circular(12),
+          //   ),
+          //   child: GestureDetector(
+          //     onTap: () => delete(),
+          //     child: const ListTile(
+          //       title: Text("Delete Account"),
+          //       trailing: (Icon(
+          //         CupertinoIcons.trash,
+          //         color: Colors.black,
+          //       )),
+          //     ),
+          //   ),
+          // )
         ],
       ),
     );
